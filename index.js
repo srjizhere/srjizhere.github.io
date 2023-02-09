44d6042cc5b382b1fc71edc68b60680cdfc49233
srjizhere <bhiapatil@gmail.com>
2023-02-10 01:46:21 +0530

    projectsectiondone

---
+++ index.js
@@ -0,0 +1,20 @@
+function fasionGithub(){
+    window.open("https://github.com/srjizhere/melodic-love-4895");
+}
+function fasiondeploy(){
+    window.open("https://melodic-churros-e6ddd9.netlify.app/");
+}
+
+
+function modesensGithub(){
+    window.open("https://github.com/srjizhere/divine-society-1574");
+}
+function modsensdeploy(){
+    window.open("https://superlative-moxie-f98da0.netlify.app/");
+}
+function bellavitaGithub(){
+    window.open("https://github.com/Rafayal383/dreary-hand-8237");
+}
+function bellavitaDeploy(){
+    window.open("https://sparkling-sable-5e1c8b.netlify.app/");
+}
