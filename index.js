20edc0004d453317b3a455122602695911aeedac
srjizhere <bhiapatil@gmail.com>
2023-02-10 20:29:50 +0530

    resume_download

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function resume(){
+    window.open("https://drive.google.com/file/d/1ITlXMR5ip-_ErtLaNShVX8u2ZkjUF7cK/view?usp=share_link")
+}
+
+
+
+
+
+
+
+
+
 function fasionGithub(){
     window.open("https://github.com/srjizhere/melodic-love-4895");
 }
@@ -17,4 +29,21 @@ function bellavitaGithub(){
 }
 function bellavitaDeploy(){
     window.open("https://sparkling-sable-5e1c8b.netlify.app/");
+}
+
+function githubdir(){
+    window.open("https://github.com/srjizhere")
+}
+function linkedindir(){
+    window.open("https://www.linkedin.com/in/srjizhere/")
+}
+function phonenumber(){
+    let no = document.getElementById('phno').innerText
+    navigator.clipboard.writeText(no)
+    alert("phone number copied ClipBoard")
+}
+function gmaildir(){
+    let no = document.getElementById('gmaildir').innerText
+    navigator.clipboard.writeText(no)
+    alert("Email address copied to Clipboard")
 }
